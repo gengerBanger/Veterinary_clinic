aceba00dc5f1c25b5cd6141cd23a825f72f6eda8
Sahsa Snikers <aleksandrsarajkin95@gmail.com>
2024-04-30 13:55:07 +0300

    add patch

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,7 @@
 from typing import List
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
+from sqlalchemy import update
 
 from backend.data_base import SessionLocal
 from backend.models import GetDog, PostDog, GetRegistration
@@ -31,4 +32,10 @@ def create_new_dog(new_dog: PostDog, db: Session = Depends(get_session)):
 def get_dog_by_id(dog_id: int, db: Session = Depends(get_session)):
     return db.query(Dogs).filter_by(dog_id=dog_id).all()
 
-
+@app.patch('/dog/{dog_id}', response_model=GetDog)
+def update_dog(dog_id: int, new_dog: PostDog, db: Session = Depends(get_session)):
+    db.execute(update(Dogs).where(Dogs.dog_id == dog_id)
+               .values(name_=new_dog.name_, kind_=new_dog.kind_.value))
+    db.commit()
+    update_dog = GetDog(dog_id=dog_id, name_=new_dog.name_, kind_=new_dog.kind_.value )
+    return update_dog
